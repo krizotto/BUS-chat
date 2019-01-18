fad175961401a3f1aa1d9b1cae389fd3bd072655
mechanicznapomarancza <kzawadka@gmail.com>
2019-01-18 02:19:40 +0100

    Deleted unnecessary comments, request-response events, var->let

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-var express = require('express'),
+let express = require('express'),
   app = express(),
   server = require('http').createServer(app),
   io = require('socket.io').listen(server);
 
-server.listen(process.env.PORT || 5000);
-app.use(express.static(__dirname + '/public'));
+server.listen(process.env.PORT || 5000); //choose port
+
+app.use(express.static(__dirname + '/public')); //directory to static files (*.css, *.js)
 app.get('/',function(req,res){
   res.sendFile(__dirname+'/index.html');
-});
+}); //redirect to index.html on main page
 
 
 io.sockets.on('connection', function (socket) {
 
-   console.log("Socket connected.");
+  console.log("Socket connected.");
 
-  socket.on('connection-established', function(json){
+  socket.on('connection-request', function(json){
     console.log(json)
-    io.emit('my connection', json)
+    io.emit('connection-response', json)
   })
 
-  socket.on('my event', function(json){
+  socket.on('message-request', function(json){
     console.log(json)
     console.log('message: ' + json['message'])
-    io.emit('my response', json)
+    io.emit('message-response', json)
   })
   
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,36 +1,36 @@
 //Locals
 let this_usr;
 let socket = io.connect('http://' + document.domain + ':' + location.port);
-var key = CryptoJS.enc.Utf8.parse('1234567890123456');
+let key = CryptoJS.enc.Utf8.parse('1234567890123456');
 console.log('ssij paue')
 function encrypt(msgString, key) {
     // msgString is expected to be Utf8 encoded
-    var iv = CryptoJS.lib.WordArray.random(16);
-    var encrypted = CryptoJS.AES.encrypt(msgString, key, {
+    let iv = CryptoJS.lib.WordArray.random(16);
+    let encrypted = CryptoJS.AES.encrypt(msgString, key, {
         iv: iv
     });
     return iv.concat(encrypted.ciphertext).toString(CryptoJS.enc.Base64);
 }
 
 function decrypt(ciphertextStr, key) {
-    var ciphertext = CryptoJS.enc.Base64.parse(ciphertextStr);
+    let ciphertext = CryptoJS.enc.Base64.parse(ciphertextStr);
 
     // split IV and ciphertext
-    var iv = ciphertext.clone();
+    let iv = ciphertext.clone();
     iv.sigBytes = 16;
     iv.clamp();
     ciphertext.words.splice(0, 4); // delete 4 words = 16 bytes
     ciphertext.sigBytes -= 16;
 
     // decryption
-    var decrypted = CryptoJS.AES.decrypt({ciphertext: ciphertext}, key, {
+    let decrypted = CryptoJS.AES.decrypt({ciphertext: ciphertext}, key, {
         iv: iv
     });
     return decrypted.toString(CryptoJS.enc.Utf8);
 }
 
 socket.on('connect', function () {
-    socket.emit('connection-established', {
+    socket.emit('connection-request', {
         data: 'User Connected'
     })
 
@@ -48,24 +48,11 @@ socket.on('connect', function () {
         if (user_name !== '' && user_input != '') {
             this_usr = user_name
 
-
-
-            // var key = "6Le0DgMTAAAAANok"; //length=22
-            // var iv  = "mHGFxENnZLbienLy"; //length=22
-            //
-            // key = CryptoJS.enc.Base64.parse(key);
-            //
-            // iv = CryptoJS.enc.Base64.parse(iv);
-
-
-
             let my_input = encrypt(user_input,key);
-
-           // user_input = CryptoJS.AES.encrypt(user_input, key).toString();
 
             console.log("message after decryption: " + user_input);
 
-            socket.emit('my event', {
+            socket.emit('message-request', {
                 user_name: user_name,
                 message: my_input
             })
@@ -82,37 +69,29 @@ socket.on('connect', function () {
     })
 })
 
-socket.on('my response', function (msg) {
-
-            // var key = "6Le0DgMTAAAAANok"; //length=22
-            // var iv  = "mHGFxENnZLbienLy"; //length=22
-            //
-            // key = CryptoJS.enc.Base64.parse(key);
-            // iv = CryptoJS.enc.Base64.parse(iv);
+socket.on('message-response', function (msg) {
 
     console.log(msg)
-
     $('h3').remove()
 
     let user = decodeURIComponent(escape(msg.user_name));
     let message = decodeURIComponent(escape(msg.message));
 
     console.log("message before decryption: " + message);
-    // let bytes = CryptoJS.AES.decrypt(message, key, { iv: iv });
-    // //let bytes = CryptoJS.AES.decrypt(message.toString(), key);
-    // let mymessage = bytes.toString(CryptoJS.enc.Utf8);
+   
     let mymessage = decrypt(message,key);
+
     //Adding message bubbles
     if (user == this_usr) { //my bubbles
-        $('p.me-sign').remove()
+        $('p.me-sign').remove() //remove prev signature
         $('ul.mess').append('<li class="me">' + mymessage + '</li><br><p class="me-sign">' + user + '</p>')
     } else { //other bubbles
-        $('p.him-sign').remove()
+        $('p.him-sign').remove() //remove prev signature
         $('ul.mess').append('<li class="him">' + mymessage + '</li><br><p class="him-sign">' + user + '</p>')
     }
 })
 
-socket.on('my connection', function (msg) {
+socket.on('connection-response', function (msg) {
     console.log(msg)
 })
 
